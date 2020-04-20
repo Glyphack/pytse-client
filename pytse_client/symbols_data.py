53a4539fe1efec4618be4be61218259c8d56e6cd
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-20 17:04:12 +0430

    add utils for symbols name and index

---
+++ pytse_client/symbols_data.py
@@ -0,0 +1,22 @@
+import json
+from typing import Set, Dict
+
+from pytse_client import config
+
+ticker_name_to_index_mapping = None
+
+
+def tickers_dict() -> Dict:
+    global ticker_name_to_index_mapping
+    if ticker_name_to_index_mapping is None:
+        with open(f"{config.pytse_dir}/data/symbols_name.json", "r") as symbols_name:
+            ticker_name_to_index_mapping = json.load(symbols_name)
+    return ticker_name_to_index_mapping
+
+
+def get_ticker_index(ticker_symbol: str):
+    return tickers_dict()[ticker_symbol]
+
+
+def all_symbols() -> Set:
+    return set(tickers_dict().keys())
