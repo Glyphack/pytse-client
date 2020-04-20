09fe3dd31ad7d1a9057762279703b8846c993d36
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-20 19:56:34 +0430

    change directory

--- pytse_client/config.py
+++ pytse_client/config.py
@@ -3,5 +3,5 @@ import os
 TSE_TICKER_ADDRESS = (
     "http://tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
 )
-SYMBOLS_DATA_BASE_PATH = "symbols_data"
+DATA_BASE_PATH = "tickers_data"
 pytse_dir = os.path.dirname(__file__)
