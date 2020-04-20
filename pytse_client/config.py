fa439d2bd2810149f4d601e2799db7f7ef53bd05
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-20 17:02:53 +0430

    add pytse_dir

--- pytse_client/config.py
+++ pytse_client/config.py
@@ -1,6 +1,7 @@
+import os
+
 TSE_TICKER_ADDRESS = (
     "http://tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
 )
-BASE_PATH = "symbols_data"
-
-
+SYMBOLS_DATA_BASE_PATH = "symbols_data"
+pytse_dir = os.path.dirname(__file__)
