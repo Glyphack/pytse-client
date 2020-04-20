d7f9e76d4e0df25940b4a42212bdccd1ba804d53
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-20 17:04:29 +0430

    fix Ticker class

--- pytse_client/ticker.py
+++ pytse_client/ticker.py
@@ -1,43 +1,21 @@
-import re
-
 import pandas as pd
-from requests import HTTPError
 
 from pytse_client import config, download
-from pytse_client.utils import requests_retry_session
 
 NAME_REGEX = r"LVal18AFC='([\D]*)',"
 
 
 class Ticker:
-    def __init__(self, index: str):
+    def __init__(self, symbol: str):
         self._history: pd.DataFrame = pd.DataFrame()
-        self._index = index
+        self._symbol = symbol
 
     def from_web(self):
-        self._history = download(self._index)[self._index]
+        self._history = download(self._symbol)[self._symbol]
 
-    def from_file(self, name: str = None, base_path: str = config.BASE_PATH):
-        if name is None:
-            name = self._index
-        self._history = pd.read_csv(f"{base_path}/{name}.csv")
+    def from_file(self, base_path: str = config.SYMBOLS_DATA_BASE_PATH):
+        self._history = pd.read_csv(f"{base_path}/{self._symbol}.csv")
 
     @property
     def history(self):
         return self._history
-
-    @property
-    def symbol(self):
-        return re.findall(NAME_REGEX, self.home_response().text)[0]
-
-    def home_url(self):
-        return config.TSE_TICKER_ADDRESS.format(self._index)
-
-    def home_response(self):
-        try:
-            response = requests_retry_session().get(
-                self.home_url(), timeout=10
-            )
-            response.raise_for_status()
-        except HTTPError:
-            return self.home_response()
