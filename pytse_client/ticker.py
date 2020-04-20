b000ddc48cb182330cbb0ef6a85dd763eb33886e
glyphack <sh.hooshyari@cafebazaar.ir>
2020-04-20 20:25:12 +0430

    refactor code

--- pytse_client/ticker.py
+++ pytse_client/ticker.py
@@ -8,9 +8,11 @@ from pytse_client import config, download, symbols_data
 
 class Ticker:
     def __init__(self, symbol: str):
+        self.symbol = symbol
+        self.url = tse_settings.TSE_TICKER_ADDRESS.format(self._index)
+        self._index = symbols_data.get_ticker_index(self.symbol)
+        self.csv_path = f"{config.DATA_BASE_PATH}/{self.symbol}.csv"
         self._history: pd.DataFrame = pd.DataFrame()
-        self._symbol = symbol
-        self._index = symbols_data.get_ticker_index(self._symbol)
         if os.path.exists(self.csv_path):
             self.from_file()
         else:
@@ -20,16 +22,8 @@ class Ticker:
     def history(self):
         return self._history
 
-    @property
-    def csv_path(self):
-        return f"{config.DATA_BASE_PATH}/{self._symbol}.csv"
-
-    @property
-    def url(self):
-        return tse_settings.TSE_TICKER_ADDRESS.format(self._index)
-
     def from_web(self):
-        self._history = download(self._symbol)[self._symbol]
+        self._history = download(self.symbol)[self.symbol]
 
     def from_file(self):
         self._history = pd.read_csv(self.csv_path)
