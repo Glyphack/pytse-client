022fc7dadcedb748d5827014669d695f6b309268
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-21 00:17:30 +0430

    fix encoding problem on windows

--- pytse_client/symbols_data.py
+++ pytse_client/symbols_data.py
@@ -9,7 +9,11 @@ ticker_name_to_index_mapping = None
 def tickers_dict() -> Dict:
     global ticker_name_to_index_mapping
     if ticker_name_to_index_mapping is None:
-        with open(f"{config.pytse_dir}/data/symbols_name.json", "r") as symbols_name:
+        with open(
+                f"{config.pytse_dir}/data/symbols_name.json",
+                "r",
+                encoding="utf8"
+        ) as symbols_name:
             ticker_name_to_index_mapping = json.load(symbols_name)
     return ticker_name_to_index_mapping
 
