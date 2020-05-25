de4991a93fdf4171fab29cc18f7e60302e0cf456
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-25 12:45:40 +0430

    :wrench: add TSE_ISNT_INFO_URL

--- pytse_client/tse_settings.py
+++ pytse_client/tse_settings.py
@@ -1,6 +1,9 @@
 TSE_TICKER_EXPORT_DATA_ADDRESS = (
     "http://tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
 )
-TSE_TICKRER_ADDRESS = (
+TSE_TICKER_ADDRESS = (
     "http://tsetmc.com/Loader.aspx?ParTree=151311&i={}"
+)
+TSE_ISNT_INFO_URL = (
+    "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=57+"
 )
