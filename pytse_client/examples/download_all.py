813e4b071b9587f979555663f256bb7a8c527c6a
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-25 12:53:05 +0430

    add explanation for ticker example

--- pytse_client/examples/download_all.py
+++ pytse_client/examples/download_all.py
@@ -1,3 +1,7 @@
+"""
+This example is about downloading all tickers csv data.
+"""
+
 from pytse_client.download import download
 
 download(symbols="all", write_to_csv=True, base_path="hey")
--- pytse_client/examples/ticker.py
+++ pytse_client/examples/ticker.py
@@ -1,12 +1,19 @@
+"""
+This example is about getting data for a ticker.
+use this example if you want to get data for one ticker
+"""
+
 from pytse_client import Ticker, download
 
 download(symbols="وبملت", write_to_csv=True)  # optional
 ticker = Ticker("وبملت")
-print(ticker.history)
-print(ticker.title)
-print(ticker.url)
-print(ticker.group_name)
-print(ticker.eps)
-print(ticker.p_e_ratio)
-print(ticker.group_p_e_ratio)
-print(ticker.base_volume)
+print(ticker.history)  # سابقه قیمت سهم
+print(ticker.title)  # نام شرکت
+print(ticker.url)  # آدرس صفحه سهم
+print(ticker.group_name)  # نام گروه
+print(ticker.eps)  # eps
+print(ticker.p_e_ratio)  # P/E
+print(ticker.group_p_e_ratio)  # group P/E
+print(ticker.base_volume)  # حجم مبنا
+print(ticker.last_price)  # آخرین معامله
+print(ticker.adj_close)  # قیمت پایانی
