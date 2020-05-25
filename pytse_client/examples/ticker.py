1f9acab224c9b5b46e0616ded80ce81abade547a
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-25 12:46:24 +0430

    :children_crossing: update examples

--- pytse_client/examples/ticker.py
+++ pytse_client/examples/ticker.py
@@ -3,3 +3,10 @@ from pytse_client import Ticker, download
 download(symbols="وبملت", write_to_csv=True)  # optional
 ticker = Ticker("وبملت")
 print(ticker.history)
+print(ticker.title)
+print(ticker.url)
+print(ticker.group_name)
+print(ticker.eps)
+print(ticker.p_e_ratio)
+print(ticker.group_p_e_ratio)
+print(ticker.base_volume)
