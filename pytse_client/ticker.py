7531bc5092f6eb2f165c216061602216e2b406d5
glyphack <sh.hooshyari@cafebazaar.ir>
2020-05-25 12:51:01 +0430

    add more Ticker information
    title, group name, eps, p/e, group p/e, base volume, adj close and last price

--- pytse_client/ticker.py
+++ pytse_client/ticker.py
@@ -1,8 +1,16 @@
+import collections
 import os
-
+import re
+import functools
 import pandas as pd
 
 from pytse_client import config, download, symbols_data, tse_settings
+from utils import requests_retry_session
+
+RealtimeTickerInfo = collections.namedtuple(
+    'RealtimeTickerInfo',
+    ['last_price', 'adj_close']
+)
 
 
 class Ticker:
@@ -10,7 +18,8 @@ class Ticker:
         self.symbol = symbol
         self.csv_path = f"{config.DATA_BASE_PATH}/{self.symbol}.csv"
         self._index = symbols_data.get_ticker_index(self.symbol)
-        self._url = tse_settings.TSE_TICKRER_ADDRESS.format(self._index)
+        self._url = tse_settings.TSE_TICKER_ADDRESS.format(self._index)
+        self._info_url = tse_settings.TSE_ISNT_INFO_URL.format(self._index)
         self._history: pd.DataFrame = pd.DataFrame()
 
         if os.path.exists(self.csv_path):
@@ -29,6 +38,66 @@ class Ticker:
     @property
     def index(self):
         return self._index
+
+    @property
+    def title(self) -> str:
+        return re.findall(
+            r"Title='([\D]*)',", self.ticker_page_response.text
+        )[0].split("-")[0].strip()
+
+    @property
+    def group_name(self) -> str:
+        return re.findall(
+            r"LSecVal='([\D]*)',", self.ticker_page_response.text
+        )[0]
+
+    @property
+    def p_e_ratio(self) -> float:
+        return self.get_ticker_real_time_info_response().adj_close / self.eps
+
+    @property
+    def group_p_e_ratio(self):
+        return float(
+            re.findall(
+                r"SectorPE='([\d.]*)',", self.ticker_page_response.text
+            )[0]
+        )
+
+    @property
+    def eps(self) -> float:
+        return float(
+            re.findall(
+                r"EstimatedEPS='([\d]*)',", self.ticker_page_response.text
+            )[0]
+        )
+
+    @property
+    def base_volume(self):
+        return float(
+            re.findall(
+                r"BaseVol=([\d]*),", self.ticker_page_response.text
+            )[0]
+        )
+
+    @property
+    def last_price(self):
+        return self.get_ticker_real_time_info_response().last_price
+
+    @property
+    def adj_close(self):
+        return self.get_ticker_real_time_info_response().adj_close
+
+    @property
+    @functools.lru_cache()
+    def ticker_page_response(self):
+        return requests_retry_session().get(self._url, timeout=10)
+
+    def get_ticker_real_time_info_response(self) -> RealtimeTickerInfo:
+        response = requests_retry_session().get(self._info_url, timeout=5)
+        return RealtimeTickerInfo(
+            int(response.text.split()[1].split(",")[1]),
+            int(response.text.split()[1].split(",")[2])
+        )
 
     def from_web(self):
         self._history = download(self.symbol)[self.symbol]
