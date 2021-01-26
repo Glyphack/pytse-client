63f91660b512a7193eb1134894a92054a3754e04
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-01-26 19:33:24 +0330

    download_client_types_records in download_all test file

--- tests/test_download/download_all.py
+++ tests/test_download/download_all.py
@@ -3,7 +3,8 @@ This is a test file to ensure all symbols in symbols_name.json file are download
 This process takes a long time so it's not part of the unit test
 """
 
-from pytse_client.download import download
+from pytse_client import download, download_client_types_records
 
 if __name__ == "__main__":
     download(symbols="all")
+    download_client_types_records(symbols="all")
