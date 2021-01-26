c04d1cd92adafa0332aa2da5430b48e5ca7d7656
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-01-26 19:50:05 +0330

    Fix line length

--- tests/test_download/download_all.py
+++ tests/test_download/download_all.py
@@ -1,5 +1,6 @@
 """
-This is a test file to ensure all symbols in symbols_name.json file are downloadable
+This is a test file to ensure all symbols in symbols_name.json file are
+downloadable.
 This process takes a long time so it's not part of the unit test
 """
 
