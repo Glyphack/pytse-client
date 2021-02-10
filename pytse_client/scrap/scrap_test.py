22758fe800f1d8ef629b3c04b8de3e35b0d3f658
Shebeli <Shervinsaeedi78@gmail.com>
2021-02-10 23:21:26 +0330

    Added MainInfoPage class which can scrap data from red tab in tsetmc.com which can either be written to panda df or csv file.

---
+++ pytse_client/scrap/scrap_test.py
@@ -0,0 +1,26 @@
+import pytest, requests, os.path
+
+from .scrap import MainPageInfo
+from bs4 import BeautifulSoup
+
+
+@pytest.fixture
+def mpi_instance():
+    return MainPageInfo()
+
+@pytest.fixture
+def test_returns_high_transaction_data(mpi_instance):
+    headers, rows = mpi_instance.high_transaction_data()
+    assert len(headers) == len(rows[0])
+
+def test_returns_effiency_data(mpi_instance):
+    headers, rows = mpi_instance.chosen_stock_index_data()
+    assert len(headers) == len(rows[0])
+
+def test_makes_file_high_transaction(mpi_instance):
+    path = mpi_instance.high_transaction_csv()
+    assert os.path.exists(path)
+
+
+# def test_makes_file_effiency(mpi_instance):
+#     pass
