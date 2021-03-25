70b7fb16c843e9d1ec6535beb051466b01433b38
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-03-25 13:31:32 +0430

    Add e2e test for ticker

---
+++ tests/test_ticker/ticker_e2e.py
@@ -0,0 +1,31 @@
+"""
+access properties of Ticker for all symbols
+"""
+from collections import defaultdict
+
+from pytse_client import Ticker, all_symbols
+
+if __name__ == '__main__':
+    symbols_errors = defaultdict(list)
+    for symbol in all_symbols():
+        try:
+            ticker = Ticker(symbol)
+            ticker.history
+            ticker.client_types
+            ticker.title
+            ticker.url
+            ticker.group_name
+            ticker.eps
+            ticker.p_e_ratio
+            ticker.group_p_e_ratio
+            ticker.base_volume
+            ticker.last_price
+            ticker.adj_close
+            ticker.shareholders
+            ticker.shareholders.percentage.sum()
+            ticker.total_share
+        except Exception as e:
+            symbols_errors[symbol].append(e)
+    # if there is some error test will fail with all erros
+    if symbols_errors:
+        raise Exception(symbols_errors)
