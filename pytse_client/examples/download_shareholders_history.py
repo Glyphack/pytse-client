90444d57929750c1148c2c0114062c10110fc18d
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-04-23 13:50:19 +0430

    Update example

--- pytse_client/examples/download_shareholders_history.py
+++ pytse_client/examples/download_shareholders_history.py
@@ -1,7 +1,16 @@
+import datetime
 from time import sleep
-from pytse_client import Ticker
-symbols = ["شکبیر", "برکت"]
 
+from pytse_client import Ticker
+
+symbols = [
+    "شکبیر",
+    "برکت",
+]
+from_times = [
+    datetime.timedelta(days=90),
+    datetime.timedelta(days=150),
+]
 for symbol in symbols:
     print(f"downloading {symbol}")
     Ticker(symbol).get_shareholders_history().to_csv(f"str(symbol).csv")
