25bb4b08bb997f9603740cc08e2fb65e07698026
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-08-06 21:21:05 +0430

    fix function call

--- tests/test_download/download_all.py
+++ tests/test_download/download_all.py
@@ -7,5 +7,5 @@ This process takes a long time so it's not part of the unit test
 from pytse_client import download, download_client_types_records, all_symbols
 
 if __name__ == "__main__":
-    download(all_symbols)
-    download_client_types_records(all_symbols)
+    download(all_symbols())
+    download_client_types_records(all_symbols())
