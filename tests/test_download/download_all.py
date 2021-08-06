aefcedb181066da66054fdecb6cfa1fa6309112d
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-08-06 20:17:50 +0430

    download all symbols in test

--- tests/test_download/download_all.py
+++ tests/test_download/download_all.py
@@ -4,10 +4,8 @@ downloadable.
 This process takes a long time so it's not part of the unit test
 """
 
-import random
 from pytse_client import download, download_client_types_records, all_symbols
 
 if __name__ == "__main__":
-    random_symbols = random.sample(all_symbols(), 20)
-    download(symbols=random_symbols)
-    download_client_types_records(symbols=random_symbols)
+    download(all_symbols)
+    download_client_types_records(all_symbols)
