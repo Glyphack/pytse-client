8b0fbbf3abc2645a4630faa7fd8278df019ce517
Glyphack <sh.hooshyari@gmail.com>
2021-09-02 10:53:37 +0430

    feat: add new urls SYMBOLS_LIST_URL, MARKET_WATCH_INIT_URL

--- pytse_client/tse_settings.py
+++ pytse_client/tse_settings.py
@@ -1,18 +1,14 @@
 TSE_TICKER_EXPORT_DATA_ADDRESS = (
     "http://tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
 )
-TSE_TICKER_ADDRESS = (
-    "http://tsetmc.com/Loader.aspx?ParTree=151311&i={}"
-)
+TSE_TICKER_ADDRESS = ("http://tsetmc.com/Loader.aspx?ParTree=151311&i={}")
 TSE_ISNT_INFO_URL = (
     "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=57+"
 )
 TSE_CLIENT_TYPE_DATA_URL = (
     "http://www.tsetmc.com/tsev2/data/clienttype.aspx?i={}"
 )
-TSE_SYMBOL_ID_URL = (
-    "http://www.tsetmc.com/tsev2/data/search.aspx?skey={}"
-)
+TSE_SYMBOL_ID_URL = ("http://www.tsetmc.com/tsev2/data/search.aspx?skey={}")
 
 # use instrument id as argument
 TSE_SHAREHOLDERS_URL = (
@@ -23,6 +19,14 @@ TSE_SHAREHOLDERS_URL = (
 # date should be formatted like YYYYMMDD
 INSTRUMENT_DAY_INFO_URL = (
     "http://cdn.tsetmc.com/Loader.aspx?ParTree=15131P&i={index}&d={date}"
+)
+
+# returns list of all symbols
+SYMBOLS_LIST_URL = ("http://www.tsetmc.com/Loader.aspx?ParTree=111C1417")
+
+# market watch init, has initial data for market watch like symbols name etc.
+MARKET_WATCH_INIT_URL = (
+    "http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
 )
 
 DATE_FORMAT = "%Y%m%d"
