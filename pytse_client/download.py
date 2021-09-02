d6b06800c73f77c329d76c38b201ca507f31bac0
Glyphack <sh.hooshyari@gmail.com>
2021-09-02 10:52:08 +0430

    refactor: add error handling and retrying

--- pytse_client/download.py
+++ pytse_client/download.py
@@ -1,22 +1,28 @@
+import logging
 from concurrent import futures
 from io import StringIO
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 
-import pandas as pd
-from requests import HTTPError
 import jdatetime
-
+import pandas as pd
 from pytse_client import config, symbols_data, translations, tse_settings
-from pytse_client.utils import requests_retry_session
 from pytse_client.tse_settings import TSE_CLIENT_TYPE_DATA_URL
+from pytse_client.utils import requests_retry_session
+from requests import HTTPError
+from requests.sessions import Session
+from tenacity import retry, retry_if_exception_type, wait_random
+from tenacity.before_sleep import before_sleep_log
+
+logger = logging.getLogger(config.LOGGER_NAME)
 
 
 def download(
-        symbols: Union[List, str],
-        write_to_csv: bool = False,
-        include_jdate: bool = False,
-        base_path: str = config.DATA_BASE_PATH) -> Dict[str, pd.DataFrame]:
+    symbols: Union[List, str],
+    write_to_csv: bool = False,
+    include_jdate: bool = False,
+    base_path: str = config.DATA_BASE_PATH
+) -> Dict[str, pd.DataFrame]:
     if symbols == "all":
         symbols = symbols_data.all_symbols()
     elif isinstance(symbols, str):
@@ -38,7 +44,15 @@ def download(
             future_to_symbol[future] = symbol
         for future in futures.as_completed(future_to_symbol):
             symbol = future_to_symbol[future]
-            df: pd.DataFrame = future.result()
+            try:
+                df: pd.DataFrame = future.result()
+            except pd.errors.EmptyDataError as ex:
+                logger.error(
+                    f"Cannot read daily trade records for symbol: {symbol}",
+                    extra={"Error": ex}
+                )
+                continue
+
             df = df.iloc[::-1]
             df = df.rename(columns=translations.HISTORY_FIELD_MAPPINGS)
             df = df.drop(columns=["<PER>", "<TICKER>"])
@@ -59,27 +73,38 @@ def _adjust_data_frame(df, include_jdate):
     if include_jdate:
         df['jdate'] = ""
         df.jdate = df.date.apply(
-            lambda gregorian:
-            jdatetime.date.fromgregorian(date=gregorian))
+            lambda gregorian: jdatetime.date.fromgregorian(date=gregorian)
+        )
 
 
-def download_ticker_daily_record(ticker_index: str, session):
+@retry(
+    retry=retry_if_exception_type(HTTPError),
+    wait=wait_random(min=1, max=4),
+    before_sleep=before_sleep_log(logger, logging.DEBUG)
+)
+def download_ticker_daily_record(ticker_index: str, session: Session):
     url = tse_settings.TSE_TICKER_EXPORT_DATA_ADDRESS.format(ticker_index)
     response = session.get(url, timeout=10)
-    try:
-        response.raise_for_status()
-    except HTTPError:
-        return download_ticker_daily_record(ticker_index, session)
+    if 400 <= response.status_code < 500:
+        logger.error(
+            f"Cannot read daily trade records from the url: {url}",
+            extra={
+                "response": response.text,
+                "code": response.status_code
+            }
+        )
+
+    response.raise_for_status()
 
     data = StringIO(response.text)
     return pd.read_csv(data)
 
 
 def download_client_types_records(
-        symbols: Union[List, str],
-        write_to_csv: bool = False,
-        include_jdate: bool = False,
-        base_path: str = config.CLIENT_TYPES_DATA_BASE_PATH
+    symbols: Union[List, str],
+    write_to_csv: bool = False,
+    include_jdate: bool = False,
+    base_path: str = config.CLIENT_TYPES_DATA_BASE_PATH
 ):
     if symbols == "all":
         symbols = symbols_data.all_symbols()
@@ -93,18 +118,26 @@ def download_client_types_records(
             ticker_index = symbols_data.get_ticker_index(symbol)
             _handle_ticker_index(symbol, ticker_index)
             future = executor.submit(
-                download_ticker_client_types_record,
-                ticker_index
+                download_ticker_client_types_record, ticker_index
             )
             future_to_symbol[future] = symbol
         for future in futures.as_completed(future_to_symbol):
             symbol = future_to_symbol[future]
             df: pd.DataFrame = future.result()
+            # ignore failures
+            if df is None:
+                continue
             _adjust_data_frame(df, include_jdate)
             df_list[symbol] = df
             if write_to_csv:
                 Path(base_path).mkdir(parents=True, exist_ok=True)
                 df.to_csv(f'{base_path}/{symbol}.csv')
+
+    if len(df_list) != len(symbols):
+        print(
+            """could not download client types for all the symbols make 
+        sure you have what you need or re-run the function"""
+        )
     return df_list
 
 
@@ -117,28 +150,41 @@ def _handle_ticker_index(symbol, ticker_index):
             symbols_data.append_symbol_to_file(ticker_index, symbol)
 
 
-def download_ticker_client_types_record(ticker_index: str):
+@retry(
+    retry=retry_if_exception_type(HTTPError),
+    wait=wait_random(min=1, max=4),
+    before_sleep=before_sleep_log(logger, logging.DEBUG)
+)
+def download_ticker_client_types_record(ticker_index: Optional[str]):
     data = _extract_ticker_client_types_data(ticker_index)
-    client_types_data_frame = pd.DataFrame(data, columns=[
-        "date",
-        "individual_buy_count", "corporate_buy_count",
-        "individual_sell_count", "corporate_sell_count",
-        "individual_buy_vol", "corporate_buy_vol",
-        "individual_sell_vol", "corporate_sell_vol",
-        "individual_buy_value", "corporate_buy_value",
-        "individual_sell_value", "corporate_sell_value"
-    ])
+    if len(data) == "":
+        logger.warning(
+            f"Cannot create client types data for ticker {ticker_index} from data: {data}",
+            extra={"ticker_index": ticker_index}
+        )
+        return None
+    client_types_data_frame = pd.DataFrame(
+        data,
+        columns=[
+            "date", "individual_buy_count", "corporate_buy_count",
+            "individual_sell_count", "corporate_sell_count",
+            "individual_buy_vol", "corporate_buy_vol", "individual_sell_vol",
+            "corporate_sell_vol", "individual_buy_value",
+            "corporate_buy_value", "individual_sell_value",
+            "corporate_sell_value"
+        ]
+    )
     for i in [
-        "individual_buy_", "individual_sell_",
-        "corporate_buy_", "corporate_sell_"
+        "individual_buy_", "individual_sell_", "corporate_buy_",
+        "corporate_sell_"
     ]:
         client_types_data_frame[f"{i}mean_price"] = (
-                client_types_data_frame[f"{i}value"].astype(float) /
-                client_types_data_frame[f"{i}vol"].astype(float)
+            client_types_data_frame[f"{i}value"].astype(float) /
+            client_types_data_frame[f"{i}vol"].astype(float)
         )
     client_types_data_frame["individual_ownership_change"] = (
-            client_types_data_frame["corporate_sell_vol"].astype(float) -
-            client_types_data_frame["corporate_buy_vol"].astype(float)
+        client_types_data_frame["corporate_sell_vol"].astype(float) -
+        client_types_data_frame["corporate_buy_vol"].astype(float)
     )
     return client_types_data_frame
 
