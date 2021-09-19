ea209b2a9b08bc20e268c8a0270d90693f6b3dfa
alised <seddighi.ali@gmail.com>
2021-09-19 20:51:55 +0430

    Merge records of old instrument indexes to new one

--- pytse_client/scraper/symbol_scraper.py
+++ pytse_client/scraper/symbol_scraper.py
@@ -7,6 +7,7 @@ import requests
 from bs4 import BeautifulSoup
 from pytse_client import config, tse_settings
 from pytse_client.utils.persian import replace_arabic
+from pytse_client.utils import requests_retry_session
 
 logger = logging.getLogger(config.LOGGER_NAME)
 
@@ -17,6 +18,7 @@ class MarketSymbol:
     symbol: str
     name: str
     index: int
+    old: list
 
     def __hash__(self) -> int:
         """
@@ -40,12 +42,16 @@ def get_market_symbols_from_symbols_list_page() -> List[MarketSymbol]:
     table_rows = table.find_all('tr')[1:]
     for table_row in table_rows:
         row_data = table_row.find_all('td')
+        # escape old symbols
+        if row_data[7].a.text.startswith('حذف-'):
+            continue
         market_symbols.append(
             MarketSymbol(
                 code=row_data[0].text,
                 symbol=replace_arabic(row_data[6].a.text),
                 name=replace_arabic(row_data[7].a.text).replace('\u200c', ''),
                 index=row_data[6].a.get('href').partition('inscode=')[2],
+                old=[],
             )
         )
     return market_symbols
@@ -85,8 +91,69 @@ def get_market_symbols_from_market_watch_page() -> List[MarketSymbol]:
                 code=replace_arabic(data[1]),
                 symbol=replace_arabic(data[2]),
                 index=replace_arabic(data[0]),
-                name=replace_arabic(data[3]).replace('\u200c', '')
+                name=replace_arabic(data[3]).replace('\u200c', ''),
+                old=[],
             )
         )
 
     return market_symbols
+
+
+def get_old_index_of_market_symbols(
+    symbols: List[MarketSymbol]
+) -> List[MarketSymbol]:
+    """
+    get old symbols list from search page
+
+    Parameters
+    ----------
+    market_symbols : List[MarketSymbol]
+        list of market symbols.
+
+    Returns
+    -------
+    List[MarketSymbol]
+        list of market symbols.
+
+    """
+    market_symbols = []
+
+    for symbol in symbols:
+        index, old_ids = get_symbol_ids(symbol.symbol)
+        if index is None:
+            index = symbol.index
+        market_symbols.append(
+            MarketSymbol(
+                code=symbol.code,
+                symbol=symbol.symbol,
+                index=index,
+                name=symbol.name,
+                old=old_ids,
+            )
+        )
+
+    return market_symbols
+
+
+def get_symbol_ids(symbol_name: str):
+    url = tse_settings.TSE_SYMBOL_ID_URL.format(symbol_name.strip())
+    response = requests_retry_session().get(url, timeout=10)
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        raise Exception(f"{symbol_name}: Sorry, tse server did not respond")
+
+    symbols = response.text.split(';')
+    index = None
+    old_ids = []
+    for symbol_full_info in symbols:
+        if(symbol_full_info.strip() == ""):
+            continue
+        symbol_full_info = symbol_full_info.split(',')
+        if replace_arabic(symbol_full_info[0].strip()) == symbol_name:
+            if(symbol_full_info[7] == '1'):
+                index = symbol_full_info[2]  # active symbol id
+            else:
+                old_ids.append(symbol_full_info[2])  # old symbol id
+
+    return index, old_ids
--- pytse_client/symbols_data.py
+++ pytse_client/symbols_data.py
@@ -2,6 +2,7 @@ import json
 from typing import Dict, Set
 
 from pytse_client import config
+from pytse_client.scraper.symbol_scraper import MarketSymbol
 
 ticker_name_to_index_mapping = None
 
@@ -22,24 +23,39 @@ def get_ticker_index(ticker_symbol: str):
     return None
 
 
+def get_ticker_old_index(ticker_symbol: str):
+    if ticker_symbol in symbols_information():
+        return symbols_information().get(ticker_symbol)["old"].copy()
+    return []
+
+
+def get_ticker_indexes(ticker_symbol: str):
+    indexes = []
+    if ticker_symbol in symbols_information():
+        indexes.append(symbols_information().get(ticker_symbol)["index"])
+        indexes = indexes + symbols_information().get(ticker_symbol)["old"]
+    return indexes
+
+
 def all_symbols() -> Set:
     return set(symbols_information().keys())
 
 
 def append_symbol_to_file(
-    symbol_id: str,
-    symbol_name: str,
-    board: str = "",
-    industry_group_name: str = ""
+    market_symbol: MarketSymbol,
 ):
+    global ticker_name_to_index_mapping
     new_symbol = {
-        symbol_name.strip():
-            {
-                "index": symbol_id.strip(),
-                "board": board,
-                "industry_group_name": industry_group_name
+        market_symbol.symbol: {
+                "index": market_symbol.index,
+                "code": market_symbol.code,
+                "name": market_symbol.name,
+                "old": market_symbol.old
             }
     }
+    print(market_symbol)
+    if ticker_name_to_index_mapping is not None:
+        ticker_name_to_index_mapping.update(new_symbol)
     with open(
         f"{config.pytse_dir}/data/symbols_name.json", "r+", encoding="utf8"
     ) as file:
