c79630e7f81fe394bb344540449c447ba51589c1
alised <seddighi.ali@gmail.com>
2021-10-06 14:43:38 +0330

    fetch NAV value

--- pytse_client/tse_settings.py
+++ pytse_client/tse_settings.py
@@ -2,8 +2,11 @@ TSE_TICKER_EXPORT_DATA_ADDRESS = (
     "http://tsetmc.com/tsev2/data/Export-txt.aspx?t=i&a=1&b=0&i={}"
 )
 TSE_TICKER_ADDRESS = ("http://tsetmc.com/Loader.aspx?ParTree=151311&i={}")
+
+# FIXME: c should be cSecVal (group code of instrument)
+# remove e parameter when instrument has not NAV value
 TSE_ISNT_INFO_URL = (
-    "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=57+"
+    "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=0&e=1"
 )
 TSE_CLIENT_TYPE_DATA_URL = (
     "http://www.tsetmc.com/tsev2/data/clienttype.aspx?i={}"
