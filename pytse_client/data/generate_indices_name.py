fab05de5a6b905ae3190d09cad7002f448c53ae4
S. F. Maqrebi <sajad.faghfoor@gmail.com>
2022-03-18 12:16:53 +0330

    Add financial index feature as in #26 discussed (#155)
    Co-authored-by: glyphack <sh.hooshyari@gmail.com>

---
+++ pytse_client/data/generate_indices_name.py
@@ -0,0 +1,30 @@
+import requests as req
+import re
+import json
+
+
+if __name__ == '__main__':
+    text = req.get(
+        "http://www.tsetmc.com/Loader.aspx?Partree=151315&Flow=1").text
+
+    market_indexes = re.findall(
+        r'<tr><td><a target="_blank" href="Loader'
+        r'\.aspx\?ParTree=15131J&i=\d*">.*</a></td>',
+        text)
+    final_dict = {}
+
+    for market_index in market_indexes:
+        key = re.search(
+            '">(.*)</a></td>', market_index).group(1)
+        index = re.search(
+            '<tr><td><a target="_blank" href="Loader'
+            r'\.aspx\?ParTree=15131J&i=(\d*)',
+            market_index).group(1)
+        section_number = re.search(r'(^\d+|\d+$)', key)
+        section_number = int(section_number.group(
+            1)) if section_number is not None else ""
+        key = re.sub(r'^\d+|\d+$|-', '', key)
+        final_dict[key] = {"index": index, "section_number": section_number}
+
+    with open("indices_name.json", "w") as f:
+        json.dump(final_dict, f, indent=2, ensure_ascii=False)
---
+++ pytse_client/examples/download_financial_index.py
@@ -0,0 +1,11 @@
+"""
+This example is about downloading two sample financial indexes.
+"""
+
+from pytse_client.download import download_financial_indexes
+
+download_financial_indexes(
+    symbols=["شاخص قيمت 50 شركت", "فني مهندسي"],
+    write_to_csv=True,
+    include_jdate=True
+)
