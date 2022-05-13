bae798bb4c5222583615593e0849b0468db2d304
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-05-13 13:57:37 +0430

    to dataframe + change dir

--- pytse_client/key_stats.py
+++ pytse_client/key_stats.py
@@ -2,20 +2,21 @@ import json
 from pathlib import Path
 import os
 import re
+import pandas as pd
 from typing import Dict, List, Tuple
 from pytse_client import (
     utils,
     tse_settings,
     config
 )
-from pytse_client.data.utils import get_index_to_symbol_map
 
-from .filter_data import filter_key_value
+from pytse_client.ticker_statisticals.utils import get_index_to_symbol_map
+from pytse_client.ticker_statisticals import filter_key_value, filter_value_NONE
 
 
 def _map_index_to_symbols():
     parent_dir = os.path.dirname(os.path.abspath(__file__))
-    symbols_file = os.path.join(parent_dir, "symbols_name.json")
+    symbols_file = os.path.join(parent_dir, "data/symbols_name.json")
     with open(symbols_file, 'r') as json_file:
         symbol_dic = json.load(json_file)
     return get_index_to_symbol_map(symbol_dic)
@@ -50,42 +51,61 @@ def _get_list_of_processed_stats(raw_key_stats: str) \
         r'([0-9]+)\,([0-9]+)\,([0-9\.]+)\;',
         '@\\g<1>@\\g<2>,\\g<3>;', raw_key_stats)
     list_of_key_stats = proccessed_key_stats.split("@")[1:]
-    idxs = list_of_key_stats[0::2]
+    indices = list_of_key_stats[0::2]
     values = list_of_key_stats[1::2]
 
-    return idxs, values
+    return indices, values
 
 
-def get_aggregated_key_stats(base_path=None, to_json=False)\
+def get_aggregated_key_stats(base_path=None, to_json=False, to_csv=False)\
         -> Dict[str, Dict[str, str]]:
     aggregated_key_stats = {}
     index_to_symbol_map = _map_index_to_symbols()
     raw_key_stats = _request_key_stats()
-    idxs, values = _get_list_of_processed_stats(raw_key_stats)
+    indices, values = _get_list_of_processed_stats(raw_key_stats)
 
-    linked_stats = zip(idxs, values)
+    linked_stats = zip(indices, values)
     for idx_stat, val_stat in linked_stats:
         if idx_stat not in index_to_symbol_map:
             continue
         else:
             symbol = index_to_symbol_map[idx_stat]["symbol"]
             name = index_to_symbol_map[idx_stat]["name"]
-        tmp_key_stat = {}
+        filter_key_found = {}
         segmented_val_stat = re.split(r'\;', val_stat)
         segmented_val_stat = list(
             filter(lambda x: x != '', segmented_val_stat))
         for each_segment in segmented_val_stat:
             key, val = each_segment.split(",")
-            tmp_key_stat[filter_key_value[int(key)]] = val
+            filter_key_found[filter_key_value[int(key)]] = val
         aggregated_key_stats[idx_stat] = {
-            **tmp_key_stat, "symbol": symbol, "name": name}
+            **filter_value_NONE,
+            **filter_key_found, 
+            "symbol": symbol, 
+            "name": name
+        }
 
-    if to_json:
+    aggregated_key_stats_df = pd.DataFrame.from_dict(aggregated_key_stats).T
+    
+    # change type of columns to int
+    str_cols = {"symbol", "name"}
+    numeric_cols = set(aggregated_key_stats_df.columns) - str_cols
+    numeric_cols_ls = list(numeric_cols)
+    aggregated_key_stats_df[numeric_cols_ls]\
+        = aggregated_key_stats_df[numeric_cols_ls].apply(
+            pd.to_numeric, 
+            errors='coerce'
+        )
+    aggregated_key_stats_df["index"] = aggregated_key_stats_df.index
+    aggregated_key_stats_df.reset_index(drop=True, inplace=True)
+        
+    if to_csv:
         base_path = config.KEY_STATS_BASE_PATH if\
                         base_path is None else base_path
         Path(base_path).mkdir(parents=True, exist_ok=True)
-        path = os.path.join(base_path, "key_stats.json")
-        with open(path, 'w', encoding="utf8") as json_file:
-            json.dump(aggregated_key_stats, json_file,
-                      indent=4, ensure_ascii=False)
-    return aggregated_key_stats
+            
+        path = os.path.join(base_path, "key_stats.csv")
+        aggregated_key_stats_df.to_csv(path, index=False)                
+            
+    
+    return aggregated_key_stats_df
