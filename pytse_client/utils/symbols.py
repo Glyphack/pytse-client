3d2eee96ffa84d3dfc5caba8d7686e7e20e497ad
alised <seddighi.ali@gmail.com>
2022-09-17 13:33:56 +0430

    fix unicode decode error

--- pytse_client/utils/symbols.py
+++ pytse_client/utils/symbols.py
@@ -6,6 +6,6 @@ from pytse_client import config
 
 def map_index_to_symbols():
     symbols_file = f"{config.pytse_dir}/data/symbols_name.json"
-    with open(symbols_file, "r") as json_file:
+    with open(symbols_file, "r", encoding="utf8") as json_file:
         symbol_dic = json.load(json_file)
     return get_index_to_symbol_map(symbol_dic)
