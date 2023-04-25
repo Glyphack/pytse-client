1cf1b1be5857b8dddb230bfb0fcd7a2502155648
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-04-25 23:52:58 +0330

    feat: second-based orderbook

--- pytse_client/order_book.py
+++ pytse_client/order_book.py
@@ -8,6 +8,8 @@ from pytse_client.tse_settings import TICKER_ORDER_BOOK
 from pytse_client.utils.request_session import requests_retry_session
 from pytse_client.symbols_data import get_ticker_index
 
+MAX_DEPTH = 5
+
 headers = {
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US,en;q=0.9,fa-IR;q=0.8,fa;q=0.7',
@@ -18,8 +20,9 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
 }
 
+
 keys = {
-    "date": "hEven",
+    "datetime": "hEven",
     "refID": "refID",
     "depth": "number",
     "bid": "pMeDem",
@@ -30,16 +33,37 @@ keys = {
     "num_ask": "zOrdMeOf",
 }
 
+secondly_keys = [
+    "bid",
+    "ask",
+    "vol_bid",
+    "vol_ask",
+    "num_bid",
+    "num_ask",
+]
+
 
 reversed_keys = {val: key for key, val in keys.items()}
 valid_keys = [key for key in keys]
 
+def get_secondly_orderbook(symbol_name, date, to_csv=False, base_path=None):
+    df = get_orderbook(symbol_name, date)
+    new_columns = ['datetime']
+    for i in range(MAX_DEPTH):
+        columns = [
+            f"{key}_{i}" for key in secondly_keys    
+        ]
+        new_columns.extend(columns)
 
-def get_order_book(symbol_name, date, to_csv=False, base_path=None):
+    newdf = pd.DataFrame(columns=new_columns)
+    return newdf
+
+
+def get_orderbook(symbol_name, date, to_csv=False, base_path=None):
     index = get_ticker_index(symbol_name)
     date = date.strftime('%Y%m%d')
     print(f"symbol index is {index}")
-    print(f"date is {date}")
+    print(f"datetime is {date}")
     session = requests_retry_session(retries=5, backoff_factor=0.1)
     url = TICKER_ORDER_BOOK.format(
         index=index, date=date
@@ -54,10 +78,10 @@ def get_order_book(symbol_name, date, to_csv=False, base_path=None):
         return pd.DataFrame(columns=valid_keys)
     df.rename(columns=reversed_keys, inplace=True)
     df = df.loc[:, valid_keys]
-    df['date'] = pd.to_datetime(
-        date + " " + df['date'].astype(str), format='%Y%m%d %H%M%S')
-    df = df.sort_values(["date", "depth"], ascending=[True, True])
-    df.set_index("date", inplace=True)
+    df['datetime'] = pd.to_datetime(
+        date + " " + df['datetime'].astype(str), format='%Y%m%d %H%M%S')
+    df = df.sort_values(["datetime", "depth"], ascending=[True, True])
+    df.set_index("datetime", inplace=True)
 
     if to_csv:
         base_path = base_path or ORDER_BOOK_HIST_PATH
