923b2a21ecebe561fd3251a5a069254a2f0c21f1
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-04-29 00:34:51 +0330

    feat: add range queries

--- pytse_client/__init__.py
+++ pytse_client/__init__.py
@@ -1,8 +1,5 @@
 from .asks_bids import get_asks_and_bids
-from .order_book import (
-    get_orderbook,
-    get_secondly_orderbook,
-)
+from .order_book import get_orderbook
 from .download import (
     download,
     download_client_types_records,
--- pytse_client/order_book.py
+++ pytse_client/order_book.py
@@ -2,13 +2,18 @@ import os
 from pathlib import Path
 import json
 import pandas as pd
+import datetime
+import logging
 
 from pytse_client.config import ORDER_BOOK_HIST_PATH
 from pytse_client.tse_settings import TICKER_ORDER_BOOK
 from pytse_client.utils.request_session import requests_retry_session
 from pytse_client.symbols_data import get_ticker_index
+from pytse_client.ticker import Ticker
 
 MAX_DEPTH = 5
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+error_msg = "{date} is not a valid date object. Make sure it was a trade day."
 
 headers = {
     "Accept": "application/json, text/plain, */*",
@@ -47,8 +52,34 @@ reversed_keys = {val: key for key, val in keys.items()}
 valid_keys = [key for key in keys]
 
 
-def get_secondly_orderbook(symbol_name, date, to_csv=False, base_path=None):
-    df = get_orderbook(symbol_name, date)
+def get_orderbook(symbol_name, start_date, end_date=None,
+                            to_csv=False, base_path=None):
+    ticker = Ticker(symbol_name)
+    end_date = start_date if not end_date else end_date
+    if not _validate_trade_date(ticker, start_date):
+        raise Exception(error_msg.format(date=start_date))
+    if not _validate_trade_date(ticker, end_date):
+        raise Exception(error_msg.format(date=end_date))
+    all_valid_dates = []
+    for n in range((end_date - start_date).days + 1):
+        date = start_date + datetime.timedelta(n)
+        if date in ticker.trade_dates:
+            all_valid_dates.append(date)
+    result = {}
+    for valid_date in all_valid_dates:
+        df = _get_orderbook(symbol_name, valid_date,
+                                      to_csv, base_path)
+        result[valid_date.strftime("%Y-%m-%d")] = df
+        logging.info(f'successfully get orderbook in {valid_date}')
+    return result
+
+
+def _validate_trade_date(ticker: Ticker, date: datetime.datetime):
+    return date in ticker.trade_dates
+
+
+def _get_orderbook(symbol_name, date, to_csv=False, base_path=None):
+    df = _get_diff_orderbook(symbol_name, date)
     df.drop(columns=["refID"], inplace=True)
     new_columns = []
     counter = 0
@@ -78,19 +109,17 @@ def get_secondly_orderbook(symbol_name, date, to_csv=False, base_path=None):
     if to_csv:
         base_path = base_path or ORDER_BOOK_HIST_PATH
         Path(base_path).mkdir(parents=True, exist_ok=True)
-
-        path = os.path.join(base_path, "secondly_orderbook.csv")
+        file_name = f'organized_orderbook_{date.strftime("%Y-%m-%d")}.csv'
+        path = os.path.join(base_path, file_name)
         df.to_csv(path, index=False)
 
     return newdf
 
 
-def get_orderbook(symbol_name, date, to_csv=False, base_path=None):
+def _get_diff_orderbook(symbol_name, date):
     index = get_ticker_index(symbol_name)
     date = date.strftime("%Y%m%d")
-    print(f"symbol index is {index}")
-    print(f"datetime is {date}")
-    session = requests_retry_session(retries=5, backoff_factor=0.1)
+    session = requests_retry_session(retries=10, backoff_factor=0.2)
     url = TICKER_ORDER_BOOK.format(index=index, date=date)
 
     response = session.get(url, headers=headers, timeout=10)
@@ -107,12 +136,5 @@ def get_orderbook(symbol_name, date, to_csv=False, base_path=None):
     )
     df = df.sort_values(["datetime", "depth"], ascending=[True, True])
     df.set_index("datetime", inplace=True)
-
-    if to_csv:
-        base_path = base_path or ORDER_BOOK_HIST_PATH
-        Path(base_path).mkdir(parents=True, exist_ok=True)
-
-        path = os.path.join(base_path, "orderbook.csv")
-        df.to_csv(path, index=False)
 
     return df
