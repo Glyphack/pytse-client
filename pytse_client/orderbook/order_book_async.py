1556e4eecbe03bcb32df99488702e9672a70866a
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-05-02 00:13:48 +0330

    feat: async

---
+++ pytse_client/orderbook/order_book_async.py
@@ -0,0 +1,56 @@
+import asyncio
+import aiohttp
+import logging
+import datetime
+import pandas as pd
+from pytse_client.ticker import Ticker
+from pytse_client.tse_settings import TICKER_ORDER_BOOK
+from pytse_client.orderbook.common import (
+    ORDERBOOK_HEADER
+)
+
+
+def get_df_valid_dates(
+    ticker: Ticker,
+    valid_dates: list,
+):
+    return asyncio.run(
+        get_df_valid_dates_async(
+            ticker,
+            valid_dates,
+        ),
+    )
+
+
+async def get_df_valid_dates_async(
+    ticker: Ticker,
+    valid_dates: list,
+):
+    conn = aiohttp.TCPConnector(limit=25)
+    session = aiohttp.ClientSession(connector=conn)
+    tasks = []
+    for date in valid_dates:
+        tasks.append(
+            _get_diff_orderbook(ticker, date, session)
+        )
+    dates_orderbooks = await asyncio.gather(*tasks)
+    await session.close()
+    return dates_orderbooks
+
+
+async def _get_diff_orderbook(
+        ticker: Ticker,
+        date_obj: datetime.date,
+        session
+):
+    index = ticker.index
+    date = date_obj.strftime("%Y%m%d")
+    url = TICKER_ORDER_BOOK.format(index=index, date=date)
+    async with session.get(
+            url,
+            headers=ORDERBOOK_HEADER,
+            timeout=10
+    ) as response:
+        logging.info(f"successfully download raw orderbook on {date} from tse")
+        data = await response.json()
+        return {date_obj: pd.json_normalize(data["bestLimitsHistory"])}
