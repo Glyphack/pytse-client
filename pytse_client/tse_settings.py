a0c2fdf077cafd82a0dca1f77cda3d5f6e9580c3
MahdiShamisavi <mahdi.msh.595@gmail.com>
2023-05-26 19:26:31 +0330

    Old-TseTmc

--- pytse_client/tse_settings.py
+++ pytse_client/tse_settings.py
@@ -7,11 +7,11 @@ TSE_TICKER_EXPORT_DATA_ADDRESS = (
 )
 
 TSE_FINANCIAL_INDEX_EXPORT_DATA_ADDRESS = (
-    "http://www.tsetmc.com/tsev2/chart/data/Index.aspx?i={}&t=value"
+    "http://old.tsetmc.com/tsev2/chart/data/Index.aspx?i={}&t=value"
 )
 
 FINANCIAL_INDEX_EXPORT_INTRADAY_URL = (
-    "http://www.tsetmc.com/Loader.aspx?ParTree=15131J&i={}"
+    "http://old.tsetmc.com/Loader.aspx?ParTree=15131J&i={}"
 )
 
 
@@ -20,20 +20,20 @@ TSE_TICKER_ADDRESS = "http://old.tsetmc.com/Loader.aspx?ParTree=151311&i={}"
 # FIXME: c should be cSecVal (group code of instrument)
 # remove e parameter when instrument has not NAV value
 TSE_ISNT_INFO_URL = (
-    "http://www.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=0&e=1"
+    "http://old.tsetmc.com/tsev2/data/instinfofast.aspx?i={}&c=0&e=1"
 )
 TSE_TICKER_INTRODUCTION_URL = (
-    "http://www.tsetmc.com/Loader.aspx?Partree=15131V&s={}"
+    "http://old.tsetmc.com/Loader.aspx?Partree=15131V&s={}"
 )
 TSE_CLIENT_TYPE_DATA_URL = (
-    "http://www.tsetmc.com/tsev2/data/clienttype.aspx?i={}"
+    "http://old.tsetmc.com/tsev2/data/clienttype.aspx?i={}"
 )
-TSE_SYMBOL_ID_URL = "http://www.tsetmc.com/tsev2/data/search.aspx?skey={}"
+TSE_SYMBOL_ID_URL = "http://old.tsetmc.com/tsev2/data/search.aspx?skey={}"
 
-TSE_TRADE_DETAIL_URL = "http://www.tsetmc.com/tsev2/data/TradeDetail.aspx?i={}"
+TSE_TRADE_DETAIL_URL = "http://old.tsetmc.com/tsev2/data/TradeDetail.aspx?i={}"
 
 # use instrument id as argument
-TSE_SHAREHOLDERS_URL = "http://www.tsetmc.com/Loader.aspx?Partree=15131T&c={}"
+TSE_SHAREHOLDERS_URL = "http://old.tsetmc.com/Loader.aspx?Partree=15131T&c={}"
 
 # returns all the instrument data for a specific date
 # date should be formatted like YYYYMMDD
@@ -42,11 +42,11 @@ INSTRUMENT_DAY_INFO_URL = (
 )
 
 # returns list of all symbols
-SYMBOLS_LIST_URL = "http://www.tsetmc.com/Loader.aspx?ParTree=111C1417"
+SYMBOLS_LIST_URL = "http://old.tsetmc.com/Loader.aspx?ParTree=111C1417"
 
 # market watch init, has initial data for market watch like symbols name etc.
 MARKET_WATCH_INIT_URL = (
-    "http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
+    "http://old.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
 )
 
 # get day share holder data
@@ -59,16 +59,16 @@ SYMBOL_DAY_INSTRUMENT_INFO_URL = (
 )
 
 # get statistical information for all tickers
-KEY_STATS_URL = "http://www.tsetmc.com/tsev2/data/InstValue.aspx?t=a"
+KEY_STATS_URL = "http://old.tsetmc.com/tsev2/data/InstValue.aspx?t=a"
 
 # get some information about each ticker using دیده بان بازار
 MARKET_WATCH_URL = (
-    "http://www.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
+    "http://old.tsetmc.com/tsev2/data/MarketWatchInit.aspx?h=0&r=0"
 )
 
 TICKER_ORDER_BOOK = "http://cdn.tsetmc.com/api/BestLimits/{index}/{date}"
 
-CLIENT_TYPES_URL = "http://www.tsetmc.com/tsev2/data/ClientTypeAll.aspx"
+CLIENT_TYPES_URL = "http://old.tsetmc.com/tsev2/data/ClientTypeAll.aspx"
 
 DATE_FORMAT = "%Y%m%d"
 MIN_DATE = 20010321
