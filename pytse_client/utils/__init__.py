3b703b8d1fb9c44d094e89906cd3cde6bc31d24a
alised <seddighi.ali@gmail.com>
2025-02-20 14:32:33 +0330

    fix function name get_html_table_header_and_rows_and_ids in utils

--- pytse_client/utils/__init__.py
+++ pytse_client/utils/__init__.py
@@ -1,7 +1,7 @@
 from pytse_client.utils.date import datetime_range
 from pytse_client.utils.request_session import requests_retry_session
 from pytse_client.utils.scrape import (
-    get_html_table_header_and_rows,
+    get_html_table_header_and_rows_and_ids,
     get_shareholders_html_table_as_csv,
 )
 from pytse_client.utils.string import convert_to_number_if_number
